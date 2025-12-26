ac1a6f5f74338ee02c63dd3cddf7742e2a87ebdd
Javier Amaya <javieramaya@itb.cat>
2025-12-26 17:27:18 +0100

    JABAnalogClock4 Galaxy J7 resolution

---
+++ rellotges/JABAnalogClock4/js/clock.js
@@ -0,0 +1,36 @@
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
+
+const updateClock = () => {
+    const now = new Date();
+    const s = now.getSeconds();
+    const m = now.getMinutes();
+    const h = now.getHours();
+
+    const sDeg = s * 6;
+    const mDeg = m * 6 + s * 0.1;
+    const hDeg = h * 30 + m * 0.5;
+
+    sec.style.transform = `rotateZ(${sDeg}deg)`;
+    min.style.transform = `rotateZ(${mDeg}deg)`;
+    hour.style.transform = `rotateZ(${hDeg}deg)`;
+};
+
+// Activar rellotge cada segon
+setInterval(updateClock, 1000);
+updateClock();
+
+// Gestió de Tema (Switch)
+const themeToggle = document.querySelector("#theme-toggle");
+
+themeToggle.addEventListener("change", (e) => {
+    const theme = e.target.checked ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("selected-theme", theme);
+});
+
+// Estat inicial: Preferència guardada o Dark Mode per defecte
+const savedTheme = localStorage.getItem("selected-theme") || "dark";
+document.documentElement.setAttribute("data-theme", savedTheme);
+themeToggle.checked = (savedTheme === "dark");
